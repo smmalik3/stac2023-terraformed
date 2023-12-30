e0217b1d223b6a3b7ab1df13f13cd5bf3ef2bb79
Salman Malik <salmalik@deloitte.com>
2023-12-30 13:08:49 -0500

    upgraded JobDecriptionGenerator to use GPT-4, modified sendToServiceNow handler with updated static job description

--- servicenow/jobDescriptionGenerator.js
+++ servicenow/jobDescriptionGenerator.js
@@ -1,6 +1,8 @@
 function sendToChatGPT(jobDescription) {
     var prompt = constructPrompt(jobDescription);
     var response = callOpenAI(prompt);
+	// response = JSON.stringify(response)
+	response = response.choices[0].message.content;
 	current.setValue('generated_job_description', response);
 	current.update();
     return response;
@@ -9,21 +11,22 @@ function sendToChatGPT(jobDescription) {
 function constructPrompt(jobDescription) {
     // Construct your prompt using the fields of the jobDescription object
     // return 'Generate a job description for the following role:\nTitle: ' + jobDescription.title + '\nLocation: ' + jobDescription.location + '\nExperience: ' + jobDescription.yearsOfExperience + ' years\nSalary Range: ' + 'Minimum Salary=' + jobDescription.salaryMin + ' Maximum Salary=' + jobDescription.salaryMax + '\nAgency: ' + jobDescription.agency + '\nDescription:';
-	return 'Write a job description for a ' + jobDescription.agency + ' ' + jobDescription.title + ' with a ' + jobDescription.education + ' or ' + jobDescription.yearsOfExperience + ' years of experience. The job is ' + jobDescription.classification + ' located in ' + jobDescription.location + ' with a salary range of ' + jobDescription.salaryMin + ' to ' + jobDescription.salaryMax + '.';
+	return 'Write a job description for a ' + jobDescription.agency + ' ' + jobDescription.title + ' with a ' + jobDescription.education + ' or ' + jobDescription.yearsOfExperience + ' years of experience. The job will pose a ' + jobDescription.sensitivity + ' risk to national security. The job is ' + jobDescription.classification + ' located in ' + jobDescription.location + ' with a salary range of ' + jobDescription.salaryMin + ' to ' + jobDescription.salaryMax + '. The position will require ' + jobDescription.travel + ' travel.';
 }
 
-function callOpenAI(prompt) {
+function callOpenAI(messageContent) {
     try {
         var requestBody = {
-            'prompt': prompt,
-            'max_tokens': 1000 // Adjust as needed
+            'model': 'gpt-4', // Ensure this is the correct model identifier
+            'messages': [{'role': 'user', 'content': messageContent}],
+            'temperature': 0.7
         };
 
         var restMessage = new sn_ws.RESTMessageV2();
-        // restMessage.setEndpoint('https://api.openai.com/v1/engines/davinci/completions');
-		restMessage.setEndpoint('https://api.openai.com/v1/engines/text-davinci-003/completions');
+        // Update the endpoint URL to the Chat API
+        restMessage.setEndpoint('https://api.openai.com/v1/chat/completions');
         restMessage.setHttpMethod('POST');
-        restMessage.setRequestHeader('Authorization', 'Bearer <ADD API KEY HERE>');
+        restMessage.setRequestHeader('Authorization', 'Bearer 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'); // Replace with your actual API key
         restMessage.setRequestHeader('Content-Type', 'application/json');
         restMessage.setRequestBody(JSON.stringify(requestBody));
 
@@ -32,18 +35,17 @@ function callOpenAI(prompt) {
         var httpStatus = response.getStatusCode();
 
         if (httpStatus !== 200) {
-            gs.error('Error in calling OpenAI. HTTP Status: ' + httpStatus);
-            return 'Error in calling OpenAI. HTTP Status: ' + httpStatus;
+            gs.error('Error in calling OpenAI Chat API. HTTP Status: ' + httpStatus);
+            return 'Error in calling OpenAI Chat API. HTTP Status: ' + httpStatus;
         }
 
         var parsedResponse = JSON.parse(responseBody);
-        return parsedResponse.choices[0].text;
+        return parsedResponse;
     } catch (ex) {
-    gs.error('Error in callOpenAI: ' + ex.message);
-    return null;
+        gs.error('Error in callOpenAIChat: ' + ex.message);
+        return null;
     }
 }
-
 // Example usage in a ServiceNow script
 var jobDescription = {
     title: current.getValue('job_title'),
@@ -52,9 +54,12 @@ var jobDescription = {
 	salaryMin: current.getValue('minimum_salary'),
 	salaryMax: current.getValue('maximum_salary'),
     agency: current.getValue('agency'),
+	travel: current.getValue('travel_required'),
+	sensitivity: current.getValue('position_sensitivity_and_risk'),
 	education: current.getValue('preferred_education_level'),
 	classification: current.getValue('position_classfication')
 };
 
 var generatedDescription = sendToChatGPT(jobDescription);
 gs.info(generatedDescription);
+action.setRedirectURL(current);
